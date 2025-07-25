6e4c15a24f47518012934a203b2de5d5f1153ceb
Mateus <mateus.alexoliveira@gmail.com>
2025-07-24 22:14:41 -0300

    Terraform config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,14 +2,10 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "ecr_repo_name" {
+variable "repository_name" {
   default = "product-catalog-repo"
 }
 
-variable "ecs_cluster_name" {
+variable "cluster_name" {
   default = "product-catalog-cluster"
-}
-
-variable "ecr_url" {
-  description = "ECR URL for the product catalog container"
 }
