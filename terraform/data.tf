8ecf643bbe73ca6e8b1aef050adfc1f3a3546c6f
Mateus <mateus.alexoliveira@gmail.com>
2025-07-24 22:25:04 -0300

    Terraform config

---
+++ terraform/data.tf
@@ -0,0 +1,21 @@
+data "aws_vpc" "default" {
+  filter {
+    name   = "isDefault"
+    values = ["true"]
+  }
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
